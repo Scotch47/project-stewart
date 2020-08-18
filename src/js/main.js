a16720b6c7dbccaeb43347189883474fa6f2ae6e
Vitaliy.Ratnikov <scotch47@yandex.ru>
2020-08-18 22:21:30 +0300

    add registration page+add select2

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,58 @@
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        let reader = new FileReader();
+$(function () {
 
-        reader.onload = function (e) {
-            $('#file_upload')
-                .attr('src', e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#file_upload')
+                    .attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-}
+
+
+
+    // select 2
+// select state
+    function formatState(state) {
+        if (!state.id) {
+            return state.text;
+        }
+        console.log(state.element.dataset);
+        var $state = $(
+            '<span><img src="' + state.element.dataset.img_src.toLowerCase() + '" class="img-flag"  /> ' + state.text + '</span>'
+        );
+        return $state;
+
+    };
+
+    $('.country-select').select2({
+        templateResult: formatState,
+        templateSelection: formatState,
+
+    });
+
+    // select payment    
+    function formatPayment(payment) {
+        if (!payment.id) {
+            return payment.text;
+        }
+        var $payment = $(
+            '<span><img src="' + payment.element.dataset.img_src.toLowerCase() + '" class="img-payment"  /> ' + payment.text + '</span>'
+        );
+        return $payment;
+
+    };
+
+    $('.payment-select').select2({
+        templateResult: formatPayment,
+        templateSelection: formatPayment,
+    });
+
+
+    
+
+});
